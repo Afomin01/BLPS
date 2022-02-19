84a0583b6f0984ec0b3e2be24598bf43a1db9caf
Afomin <284718@niuitmo.ru>
2022-02-19 22:41:33 +0300

    Fixed UserService.java

--- src/main/java/com/example/blps/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/example/blps/configuration/WebSecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.blps.configuration;
 
-import com.example.blps.service.impl.SpringUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -20,9 +20,9 @@ import org.springframework.security.web.firewall.HttpFirewall;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private final SpringUserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
-    public WebSecurityConfiguration(final SpringUserDetailsService userDetailsService) {
+    public WebSecurityConfiguration(final UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
 
--- src/main/java/com/example/blps/service/impl/UserService.java
+++ src/main/java/com/example/blps/service/impl/UserService.java
@@ -5,12 +5,14 @@ import com.example.blps.model.User;
 import com.example.blps.repository.UserRepository;
 import com.example.blps.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class UserService implements IUserService {
+public class UserService implements IUserService, UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -23,5 +25,14 @@ public class UserService implements IUserService {
     @Override
     public User createNewUser(UserCreateDTO createDTO) {
         return null;
+    }
+
+    @Override
+    public User loadUserByUsername(String username) {
+        return userRepository.
+                findByUsername(username).
+                orElseThrow(
+                        () -> new UsernameNotFoundException("User with username ")
+                );
     }
 }
