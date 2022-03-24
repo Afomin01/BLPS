f132a91c6ff1752893f9ea3727aa8b542ba8e6b3
Afomin <284718@niuitmo.ru>
2022-03-24 22:53:45 +0300

    Added scheduling

--- src/main/java/com/example/blps/repository/QuestionRepository.java
+++ src/main/java/com/example/blps/repository/QuestionRepository.java
@@ -4,11 +4,20 @@ import com.example.blps.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface QuestionRepository extends JpaRepository<Question, UUID> {
     @Query(value = "select q.id, q.title, q.text, q.creation_timeutc, q.rating, q.needs_moderation, q.user_id from Question q where q.needs_moderation = true limit 1", nativeQuery = true)
     Optional<Question> getAnyUnmoderatedQuestion();
+
+    @Query("select q from Question q where q.rating <= ?1 and q.needsModeration = false ")
+    List<Question> findUnmoderatedQuestionsByRatingLessThan(int rating);
+
+    @Query("select q from Question q where q.rating <= ?1")
+    List<Question> findQuestionsByRatingLessThan(int rating);
+
+
 
 }
